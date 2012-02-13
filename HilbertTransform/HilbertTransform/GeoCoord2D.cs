c8cbb1410b556d6bcbab3537b9755693376c766e
cjw39 <cjw39@hotmail.com>
2012-02-13 21:46:59 +0000

    Initial add of project files into repository.

---
+++ HilbertTransform/HilbertTransform/GeoCoord2D.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HilbertTransform
+{
+    public class GeoCoord2D : Point2D
+    {
+        public GeoCoord2D(double lat, double lon) :
+            base(lon, lat)
+        {
+            if (!LatIsValid(lat))
+                throw new ArgumentException("Latitude is out of range.");
+            if (!LonIsValid(lon))
+                throw new ArgumentException("Longitude is out of range.");
+        }
+
+        public double Latitude
+        {
+            get { return base.y; }
+            set
+            {
+                if (!LatIsValid(value))
+                    throw new ArgumentException("Latitude is out of range.");
+                base.y = value;
+            }
+        }
+
+        public double Longitude
+        {
+            get { return base.x; }
+            set
+            {
+                if (!LonIsValid(value))
+                    throw new ArgumentException("Longitude is out of range.");
+                base.x = value;
+            }
+        }
+
+        public override double X
+        {
+            get
+            {
+                return base.X;
+            }
+            set
+            {
+                Longitude = value;
+            }
+        }
+
+        public override double Y
+        {
+            get
+            {
+                return base.Y;
+            }
+            set
+            {
+                Latitude = value;
+            }
+        }
+
+        public static bool LatIsValid(double lat)
+        {
+            if (lat < -90 || lat > 90)
+                return false;
+            return true;
+        }
+
+        public static bool LonIsValid(double lon)
+        {
+            if (lon < -180 || lon > 180)
+                return false;
+            return true;
+        }
+    }
+}
---
+++ HilbertTransform/HilbertTransform/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HilbertTransform
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int quadIndex = HCTransform.GetQuadIndex(new Point2D(10, 0),
+            //    new Point2D(0, 0), new Point2D(10, 10));
+            //Console.WriteLine(quadIndex);
+
+//            int v = HCTransform.Transform(new Point2D(-1f, -4f),
+//                new Point2D(-4f, -4f), new Point2D(4f, 4f),
+//                3, CurveType.LeftTop);
+//            Console.WriteLine(v);
+
+            //HCTransform.DrawCurveKml(new Point2D(-85.59, 32.51),
+            //    new Point2D(-85.41, 32.69), 7, CurveType.UpLeft);
+
+            Console.WriteLine(HCTransform.Transform(
+                new Point2D(-85.481880, 32.608915),
+                new Point2D(-85.59, 32.51),
+                new Point2D(-85.41, 32.69),
+                7,
+                CurveType.UpLeft));
+            Console.WriteLine(HCTransform.Transform(
+                new Point2D(-85.480510, 32.609240),
+                new Point2D(-85.59, 32.51),
+                new Point2D(-85.41, 32.69),
+                7,
+                CurveType.UpLeft));
+            Console.WriteLine(HCTransform.Transform(
+                new Point2D(-85.479774, 32.608709),
+                new Point2D(-85.59, 32.51),
+                new Point2D(-85.41, 32.69),
+                7,
+                CurveType.UpLeft));
+            Console.WriteLine(HCTransform.Transform(
+                new Point2D(-85.481590, 32.610287),
+                new Point2D(-85.59, 32.51),
+                new Point2D(-85.41, 32.69),
+                7,
+                CurveType.UpLeft));
+
+            Console.WriteLine(HCTransform.Transform(
+                new Point2D(-85.5345952175, 32.55961729),
+                new Point2D(-85.59, 32.51),
+                new Point2D(-85.41, 32.69),
+                7,
+                CurveType.UpLeft));
+
+            Console.ReadLine();
+        }
+    }
+}
